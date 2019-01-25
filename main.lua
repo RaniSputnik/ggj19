704a60473268f14d5c99d98e280fcd8f1fb300c2
Ryan Loader <talk@ryanloader.me>
2019-01-25 21:40:40 +0000

    Add continue to skip text reveal

--- main.lua
+++ main.lua
@@ -7,12 +7,21 @@ state_speak = require('state_speak')
 
 state = state_speak({ speech = 'Hello, nice to meet you!' })
 
+input = {
+    continue = false,
+    continue_pressed = false
+}
+
 function love.load()
     R.fonts.speech = love.graphics.newFont('assets/fonts/GentiumPlus-R.ttf', 32, "normal")
 end
 
 function love.update(dt)
-    state = state.update(dt * GAME_SPEED)
+    local was_continue = input.continue
+    input.continue = love.keyboard.isDown('space')
+    input.continue_pressed = not was_continue and input.continue
+
+    state = state.update(input, dt * GAME_SPEED)
 end
 
 function love.draw()
--- state_speak.lua
+++ state_speak.lua
@@ -4,13 +4,22 @@ return function(params)
         current_pos = 0
     }
 
-    state.update = function(dt)
-        if state.current_pos >= string.len(state.full_speech) then
+    state.update = function(input, dt)
+        local total_speech_length = string.len(state.full_speech)
+
+        -- We have already shown all of the text
+        if state.current_pos >= total_speech_length then
             state.current_speech = state.full_speech
+        -- The player has used the 'continue' button
+        elseif input.continue_pressed then
+            state.current_pos = total_speech_length
+            state.current_speech = state.full_speech
+        -- The text is still slowly being revealed
         else
             state.current_pos = state.current_pos + dt
             state.current_speech = string.sub(state.full_speech, 0, state.current_pos)
         end
+
         return state
     end
 
