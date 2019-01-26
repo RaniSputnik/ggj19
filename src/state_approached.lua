9356621a3e9bca283a52d37dfd91ff8573d301c8
Ryan Loader <talk@ryanloader.me>
2019-01-26 17:34:05 +0000

    Approaching player starts a conversation

--- src/state_approached.lua
+++ src/state_approached.lua
@@ -1,4 +1,5 @@
--- The move state is where the player is freely moving
+-- The approached state is when the player is frozen while another
+-- character approaches them.
 
 return function(params)
     local state = {
@@ -6,7 +7,29 @@ return function(params)
     }
 
     state.update = function(input, dt)
-        state.target:update()
+        local px, py = world.player:getPos()
+        local tx, ty = state.target:getPos()
+
+        -- TODO a* to determine path to player
+
+        local synthesized_input = {
+            left = false,
+            right = false,
+            up = false,
+            down = false,
+        }
+        local hor = px - tx
+        local ver = py - ty
+        if hor > 0 then synthesized_input.right = true
+        elseif hor < 0 then synthesized_input.left = true
+        elseif ver > 0 then synthesized_input.down = true
+        elseif ver < 0 then synthesized_input.up = true
+        end
+
+        state.target:update(synthesized_input, dt)
+        if state.target:facing() == world.player then
+            return state_speak({ speaker = state.target, speech = 'TODO: Ha! I approached you!' })
+        end
 
         return state
     end
